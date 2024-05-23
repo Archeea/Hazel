8e8e5ee797ba3d971644023f5b96e8077f108902
20 20 <foranatomymodel20@gmail.com>
2024-05-23 13:26:01 +0530

    it

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+// for use by Hazel applications
+#include<stdio.h>
+#include "Hazel/Application.h"
+#include "Hazel/log.h"
+
+//--Entry Point ----------------
+#include "Hazel/EntryPoint.h"
+//-----------------------
---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,21 @@
+#include "Application.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
+namespace Hazel {
+	Application::Application()
+	{
+
+	};
+	Application:: ~Application()
+	{
+
+	};
+
+	void Application::Run()
+	{
+	    WindowResizeEvent e(1200, 720);
+		HZ_TRACE(e);
+		while (true);
+	}
+}
